5f0128dcb1b23dd2428e448754981ecff8b0ae3b
kellyinfortech <kellyinfortech.solution@gmail.com>
2023-10-23 15:48:19 -0700

    exception in python project

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,14 +2,11 @@
 
 def safe_print_list(my_list=[], x=0):
     sum_total = 0
-
+    for i in range(x):
         try:
-            for i in range(x):
-                print(f"{my_list[i]}", end="")
-                sum_total += 1
-
+            print(f"{my_list[i]}", end="")
+            sum_total += 1
         except IndexError:
-            pass
+            break
     print()
-
     return(sum_total)
