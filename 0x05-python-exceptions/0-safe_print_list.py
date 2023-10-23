6c2b45a0db98855a8c178d71131ed17fbe494ed9
kellyinfortech <kellyinfortech.solution@gmail.com>
2023-10-23 16:17:08 -0700

    exception in python project

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,14 +1,12 @@
 #!/usr/bin/python3
-
-
 def safe_print_list(my_list=[], x=0):
     i = 0
-    rec = 0
+    printed = 0
     for i in range(0, x):
         try:
             print("{}".format(my_list[i]), end="")
-            red += 1
+            printed += 1
         except:
             continue
     print()
-    return rec
+    return printed
